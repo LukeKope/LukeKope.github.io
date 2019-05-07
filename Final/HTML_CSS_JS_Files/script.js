b55a837e046c32b8652bed36999e32a703039646
Luke Koppenheffer <luke.koppenheffer@nyu.edu>
2019-05-07 06:48:38 -0400

    final commit

--- Final/HTML_CSS_JS_Files/script.js
+++ Final/HTML_CSS_JS_Files/script.js
@@ -16,22 +16,4 @@ $(document).ready(function() {
 	$('.fadeContainer').animate({'opacity':'1'},1000);
 	$('.aboutUsButton').delay(1300).animate({'marginTop':'+=-10px','opacity':'1'},800);
 
-
-//Time permiting have it so that the logo fades in first (or last, see which looks better). To do this, need to isolate all of the elements you want to fade in and fade them all individually rather than
-//using this fade container.
-
-
 });
-
-//Have the navbar section change colors when you're on that section (toDo save this to the end. It's a small detail)
-function activeChange() {
-	console.log('test');
-  var elmnt = document.getElementById("about");
-  var aboutHeight = elmnt.clientHeight;
-	var screenHeight = window.innerHeight;
-	console.log(aboutHeight, screenHeight);
-	if(screenHeight > aboutHeight){
-		console.log('height');
-	}
-}
-//use the client height for this function it's what you can see on the screen
